42530f321627119287e876b733beb66c6600a6a6
IldarGabi <IldarGabi@gmail.com>
2021-02-28 02:16:11 +0300

    Create code by Test Task

---
+++ src/test/java/test/OrderForm.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OrderForm {
+    @BeforeEach
+    void setup() {
+        open("http://computer-database.gatling.io/computers/new");
+    }
+
+    @Test
+    void orderAutoFormWithSelenide() {
+        String introducedDate = LocalDate.now().minusYears(5).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String discontinuedDate = LocalDate.now().minusYears(4).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String name = "Гена";
+        $("[id=name]").setValue(name);
+        $("[id=introduced]").setValue(introducedDate);
+        $("[id=discontinued]").setValue(discontinuedDate);
+        $("[id=company]").click();
+        $$("[name=company]").first().find(withText("IB")).click();
+        $("[type=submit]").click();
+        $("[class='alert-message warning']").waitUntil(Condition.visible, 2000)
+                .shouldHave(exactText("Done !  Computer " + name + " has been created "));
+    }
+}
